d89c8443cd8df9c1e61a459fc64f99a462155dbe
stefanbauer <alternative2k@googlemail.com>
2021-07-17 15:32:07 +0200

    added for loop

--- src/main/java/Training.java
+++ src/main/java/Training.java
@@ -1,15 +1,26 @@
 public class Training {
     public static void main(String[] args) {
 
-        System.out.println(add(11,6,3));
+       // System.out.println(add(11,6,3));
 
+        System.out.println(forLoop());
     }
+
     public static int add(int number1,int number2, int number3) {
 
-
-        int result = number1 - number2 - number3;
+        int result = number1 + number2 - number3;
         return result;
+    }
 
+    public static int forLoop() {
+
+        int inputX = 6;
+        int[] zahlen = new int[4];
+
+        for(int i = 0; i < zahlen.length ; i++) {
+            zahlen[i] = inputX;
+        }
+        return zahlen[0];
     }
 
 
--- src/test/java/TrainingTest.java
+++ src/test/java/TrainingTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrainingTest {
 
+    @Test
+    @DisplayName("return must be 20")
+    public void addTest() {
+        // given
+        int input1 = 11;
+        int input2 = 6;
+        int input3 = 3;
+        // when
+        int actual = Training.add(input1, input2, input3);
+        //then
+        assertEquals(20, actual);
+    }
 }
