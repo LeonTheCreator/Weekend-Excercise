57ba393994c062dcec8ed769e2707bf098e84c1a
Leontiy Shcherbyna <leontiy@Leontiys-MBP.fritz.box>
2021-07-17 14:23:45 +0200

    Training!

---
+++ src/main/java/Training.java
@@ -0,0 +1,18 @@
+public class Training {
+    public static void main(String[] args) {
+
+        System.out.println(add(11,6,3));
+
+    }
+    public static int add(int number1,int number2, int number3) {
+
+
+        int result = number1 - number2 - number3;
+        return result;
+
+    }
+
+
+
+}
+
